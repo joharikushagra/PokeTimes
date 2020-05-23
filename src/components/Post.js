1d9475929f4da8147dcbde7e2e09b9530a8776d9
Kushagra Johari <skjkushagra1309@gmail.com>
2020-05-23 19:32:53 +0530

    added accessing dynamic url functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
  class Post extends Component {
      constructor(props) {
          super(props)
      
          this.state = {
-              id: null
+              post:null
          }
      }
      
      componentDidMount(){
          console.log(this.props)
          let id = this.props.match.params.post_id
-         this.setState({
-             id: id
+         axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
+         .then(res=>{
+            this.setState({
+                post: res.data
+            })
+         console.log(res);
+
          })
+      
      }
     render() {
-
+     const post = this.state.post ? (
+         <div className="post">
+             <h2 className="center">{this.state.post.title}</h2>
+             <p>{this.state.post.body}</p>                 
+         </div>
+     ) : (
+         <div className="center">Loading....</div>
+     )
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                <h4>{post}</h4>
             </div>
         )
     }
